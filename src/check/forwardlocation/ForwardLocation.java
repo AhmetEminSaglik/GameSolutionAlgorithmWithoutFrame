ce9d4442d84fd381233b5c282cb558750d8fc995
Ahmet Emin <ahmeteminsaglik@gmail.com>
2021-11-20 22:31:24 +0300

    drone class is created

--- src/check/forwardlocation/ForwardLocation.java
+++ src/check/forwardlocation/ForwardLocation.java
@@ -1,14 +1,9 @@
 package check.forwardlocation;
 
-import errormessage.joptionpanel.ShowPanel;
+import drone.Drone;
 import game.Game;
-import game.gamerepo.player.Player;
-import game.gamerepo.player.robot.Robot;
-import game.location.CreateLocation;
 import game.location.DirectionLocation;
-import game.location.Location;
 import game.location.LocationsList;
-import game.move.MovePlayer;
 import squareprocess.SquareProcess;
 
 import java.util.ArrayList;
@@ -16,24 +11,27 @@ import java.util.ArrayList;
 public class ForwardLocation {
     SquareProcess squareProcess = new SquareProcess();
     ArrayList<DirectionLocation> locationsList = new LocationsList().getList();
+    Drone drone;
 
     public int inspectLocationAndGetAvailableSquareNumbers(Game game, DirectionLocation locationNeededToInspect) {
 
-        Location droneLocation = getDroneLocation(game, locationNeededToInspect);
+        Drone drone = buildDrone(game, locationNeededToInspect);
 
         int availableLocationNumberToMove = 0;
         for (int i = 0; i < locationsList.size() - 1; i++) {
-            if (squareProcess.isSquareAvailableToMoveOnIt(game, droneLocation, locationsList.get(i)))
+            if (squareProcess.isSquareAvailableToMoveOnIt(game, drone.getLocation(), locationsList.get(i)))
                 availableLocationNumberToMove++;
         }
         return availableLocationNumberToMove;
     }
 
-    Location getDroneLocation(Game game, DirectionLocation locationNeededToInspect) {
-        Location droneLocation = new Location();
-        droneLocation.setX(game.getPlayer().getLocation().getX() + locationNeededToInspect.getX());
-        droneLocation.setY(game.getPlayer().getLocation().getY() + locationNeededToInspect.getY());
-        return droneLocation;
+    Drone buildDrone(Game game, DirectionLocation locationNeededToInspect) {
+
+        Drone drone = new Drone();
+        drone.getLocation().setX(game.getPlayer().getLocation().getX() + locationNeededToInspect.getX());
+        drone.getLocation().setY(game.getPlayer().getLocation().getY() + locationNeededToInspect.getY());
+
+        return drone;
     }
 
 }
--- src/drone/Drone.java
+++ src/drone/Drone.java
@@ -1,5 +1,15 @@
 package drone;
 
-public class Drone {
+import game.location.Location;
 
+public class Drone {
+    private Location location = new Location();
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public void setLocation(Location location) {
+        this.location = location;
+    }
 }
