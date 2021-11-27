8103b06f722dbba7362a9d36d7d629f3f31ce706
Ahmet Emin <ahmeteminsaglik@gmail.com>
2021-11-27 15:54:53 +0300

    image of results is added

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -13,7 +13,7 @@ public class Main {
 
     public static void main(String[] args) throws InterruptedException {
 
-        int squareEdge = 6;
+        int squareEdge = 5;
         BuildGame buildGameModel = new BuildGame(squareEdge, squareEdge);
         Game game = buildGameModel.createGame();
 
